e5b734cf63985e6b8e3f1bc0e9d229e62a62e709
Jogendra <jogendrafx@gmail.com>
2020-12-28 12:42:53 +0530

    With Import Cycle

--- p2/p2.go
+++ p2/p2.go
@@ -2,20 +2,13 @@ package p2
 
 import (
 	"fmt"
+	"import-cycle-example/p1"
 )
 
-type pp1 interface {
-	HelloFromP1()
-}
+type PP2 struct{}
 
-type PP2 struct {
-	PP1 pp1
-}
-
-func New(pp1 pp1) *PP2 {
-	return &PP2{
-		PP1: pp1,
-	}
+func New() *PP2 {
+	return &PP2{}
 }
 
 func (p *PP2) HelloFromP2() {
@@ -23,5 +16,6 @@ func (p *PP2) HelloFromP2() {
 }
 
 func (p *PP2) HelloFromP1Side() {
-	p.PP1.HelloFromP1()
+	pp1 := p1.New()
+	pp1.HelloFromP1()
 }
